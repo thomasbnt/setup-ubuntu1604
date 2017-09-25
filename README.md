6c6e143bfcc8813d9954fcdb913eb71565ecd8c6
Thomas bnt <darkmoon172m@gmail.com>
2017-09-26 00:10:43 +0200

    First Commit !

---
+++ README.md
@@ -0,0 +1,40 @@
+# Setup
+
+Install softwares for your Ubuntu 16.04 . 
+
+
+### Installing
+
+This script must be run as root 
+
+```
+sudo -s
+```
+
+## Software list
+
+* Git 
+* Gimp
+* paper-gtk-theme
+* paper-icon-theme
+* unity-tweak-tool 
+* openvpn 
+* network-manager-openvpn 
+* Discord 
+* Chromium-browser
+
+## Contributing
+
+See the list of [contributors](https://github.com/thomasbnt/setup-ubuntu1604/contributors) who participated in this project.
+
+## Authors
+
+* **Thomas Bnt** - *Initial work* - [Website](https://thomasbnt.fr/)
+
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+
+
---
+++ setup.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+if [[ $EUID -ne 0 ]]; then
+   echo "################################################################################"
+   echo "#                This script must be run as root (sudo -s)                     #" 
+   echo "################################################################################"
+   exit
+fi
+clear
+echo "################################################################################"
+echo "#                                                                              #"
+echo "#                                    SETUP                                     #"
+echo "#                                                                              #"
+echo "#          Repository : https://github.com/thomasbnt/setup-ubuntu1604          #"
+echo "#  Software list : https://github.com/thomasbnt/setup-ubuntu1604#software-list #"
+echo "#                                                                              #"
+echo "#                                                                              #"
+echo "#                                                                              #"
+echo "################################################################################"
+echo ""
+echo -n "# Do you want to install the software? Y/N or CTRL+C :  "
+read answer
+if [ "$answer" = "y" ] || [ "$answer" = "Y"  ] || [ "$answer" = "Yes"  ] || [ "$answer" = "yes"  ] || [ "$answer" = "YES" ]; then
+    echo ""
+    echo ""
+    echo "# Installing.."
+    apt-get update
+    apt install git
+    apt install gimp
+    add-apt-repository ppa:snwh/pulp
+    apt-get update
+    apt-get install paper-gtk-theme 
+    apt-get install paper-icon-theme
+    apt-get install unity-tweak-tool
+    apt install openvpn 
+    apt install network-manager-openvpn
+    wget https://discordapp.com/api/download?platform=linux&format=deb
+    apt -f install  
+    apt install chromium-browser
+    mkdir -p /etc/lightdm/lightdm.conf.d
+    touch /etc/lightdm/lightdm.conf.d/50-disable-guest.conf
+    echo "[SeatDefaults]" >> /etc/lightdm/lightdm.conf.d/50-disable-guest.conf 
+    echo "allow-guest=false" >> /etc/lightdm/lightdm.conf.d/50-disable-guest.conf 
+
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+    
+elif [ "$answer" = "n" ] || [ "$answer" = "N" ] || [ "$answer" = "No"  ] || [ "$answer" = "no"  ] || [ "$answer" = "NO" ]; then
+    echo ""
+    echo "# Hum okay. :door:"
+else
+    echo "# Error. Yes or No."
+fi
+
